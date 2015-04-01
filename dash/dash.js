664491473362f69886448a05509c4cafc80658a5
ubergesundheit <ubergesundheit@users.noreply.github.com>
2015-04-01 23:59:12 +0200

    Update dash.js

--- dash/dash.js
+++ dash/dash.js
@@ -1,4 +1,14 @@
-[].slice.call(document.getElementsByClassName("sparkline")).forEach(function(elem) {
-  var s = new Sparkline2(elem);
-  s.draw([1,3,5,2,1,6,7]);
+var dateFormatOptions = { hour: "2-digit", minute: "2-digit" };
+
+
+[].slice.call(document.getElementsByClassName("cell")).forEach(function(elem) {
+  var url = elem.getAttribute("dash-datasource");
+  var temp_attr_name = elem.getAttribute("dash-temp-attributename");
+  nanoajax.ajax(url, function (code, responseText) {
+    var data = JSON.parse(responseText);
+    var temps = data.result.map(function (item) { return item[temp_attr_name];});
+    var sparkline = new Sparkline2(elem.getElementsByClassName("sparkline")[0]);
+    sparkline.draw(temps); 
+  });
+
 });
