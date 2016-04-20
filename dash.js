aebb40aa512b87b92910a2e9a51a3d6a88ecba34
Gerald Pape <ubergesundheit@users.noreply.github.com>
2016-04-20 13:38:57 +0200

    Update dash.js

--- dash.js
+++ dash.js
@@ -32,8 +32,8 @@ var setTime = function (firstRun) {
 
 var sourceConfig = {
   "heimdb": "https://geraldpape.io/heimdb/latest",
-  "sensebox": "https://crossorigin.me/http://opensensemap.io:8000/boxes/57000b8745fd40c8196ad04c/sensors"
-  //"sensebox": "https://api.opensensemap.org/boxes/57000b8745fd40c8196ad04c/sensors"
+  //"sensebox": "https://crossorigin.me/http://opensensemap.io:8000/boxes/57000b8745fd40c8196ad04c/sensors"
+  "sensebox": "https://api.opensensemap.org/boxes/57000b8745fd40c8196ad04c/sensors"
 };
 
 var config = {
